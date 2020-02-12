3d40975bb65976395f866c4e1f41956b641316d2
sofietrovik <59829095+sofietrovik@users.noreply.github.com>
2020-02-12 12:23:58 +0100

    Update queue.h

---
+++ skeleton_project/source/queue.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int width = 3;
+int height = 4;
+int** queueMatrix = new int*[width];
+for(int i = 0; i < width; ++i)
+   queueMatrix[i] = new int[height]; //vil vi ha pekere?
+void updateQueue(){
+  for (int floor, floor < 4, floor++)
+}
